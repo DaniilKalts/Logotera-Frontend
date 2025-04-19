58470bc3b0ceb103274fd3760bb1bba1cd1e3005
noodle <nonomi2603@gmail.com>
2025-04-20 01:43:52 +0600

    add user profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,14 @@
+﻿"use client";
+
+import DefaultLayout from '../(default)/layout';
+
+export default function UserPage() {
+    return (
+        <DefaultLayout>
+            <div className="p-4">
+                <h1 className="text-2xl font-bold">User Page</h1>
+                {/* тут твой контент */}
+            </div>
+        </DefaultLayout>
+    );
+}
